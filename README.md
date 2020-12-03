65d4e9350531354eab7c1fe4d3599e8187aa4cfd
scottwcode <scottmail77@gmail.com>
2020-12-03 17:51:27 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## An interactive animated presentation
+
+This repo contains basic html, css, javascript and svg images to have an interactive presentation with some animations to illustrate different items throughout the presentation. The project builds an interactive, responsive, dynamic, web-based explainer presentation about deep learning.
---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+window.addEventListener('DOMContentLoaded', function(e) {
+  document.querySelector('hp-presentation').onclick = handlePresentationClick;
+})
+
+function handlePresentationClick(e) {
+  // alert('clicked');
+  var current = document.querySelector('hp-slide.active');
+  var next = current.nextElementSibling;
+
+  // Guard code to make sure next element is a slide. Will keep
+  // next'ing until the element is a slide (vs a div or other element).
+  while (next && next.tagName != 'HP-SLIDE') {
+    next = next.nextElementSibling;
+  }
+
+  if (next) {
+    current.classList.remove('active')
+    next.classList.add('active');
+  }
+}
