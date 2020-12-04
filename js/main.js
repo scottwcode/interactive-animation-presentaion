910a66ccab3f82104c444d13b0a24863a16b16b1
scottwcode <scottmail77@gmail.com>
2020-12-03 19:36:46 -0500

    added more slides and tweeked previous ones for smoother transitions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-
 window.addEventListener('DOMContentLoaded', function(e) {
-  document.querySelector('hp-presentation').onclick = handlePresentationClick;
-})
+  var presentation = document.querySelector('hp-presentation');
+
+  presentation.onclick = handlePresentationClick;
+  presentation.addEventListener('animationend', handleAnimationEnd, false);
+});
 
 function handlePresentationClick(e) {
-  // alert('clicked');
   var current = document.querySelector('hp-slide.active');
   var next = current.nextElementSibling;
 
@@ -18,6 +19,9 @@ function handlePresentationClick(e) {
     current.classList.remove('active')
     next.classList.add('active');
 
+    next.querySelectorAll('.match').forEach(function (el) {
+      setTimeout(function() { el.classList.remove('match'); }, 0);
+    });
     var aa= parseInt(next.getAttribute('data-autoadvance'));
 
     // If aa not is not a number
@@ -26,5 +30,14 @@ function handlePresentationClick(e) {
         handlePresentationClick(e);
       }, aa)
     }
+  }
+}
+
+function handleAnimationEnd(e) {
+  var slide = e.target.closest('hp-slide');
+  var aa = slide.getAttribute('data-autoadvance');
+
+  if (aa == 'animationend' && slide.classList.contains('active')) {
+    handlePresentationClick(e);
   }
 }
